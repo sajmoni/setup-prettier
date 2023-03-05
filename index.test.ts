c1b59a4d585b1ead8954582d472bfe89226e6667
Simon Lagos <simon.lagos.sallhed@gmail.com>
2023-03-05 12:59:32 +0100

    Run setup-prettier on this project

--- index.test.ts
+++ index.test.ts
@@ -1,16 +1,16 @@
-import fs from "node:fs/promises";
-import path from "node:path";
-import { expect, test } from "vitest";
-import { execa } from "execa";
-import { temporaryDirectory } from "tempy";
-import { PackageJson, readPackage } from "read-pkg";
-import { name } from "./package.json";
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { expect, test } from 'vitest'
+import { execa } from 'execa'
+import { temporaryDirectory } from 'tempy'
+import { PackageJson, readPackage } from 'read-pkg'
+import { name } from './package.json'
 
 test(name, async () => {
-  const directory = temporaryDirectory({ prefix: "hello-world" });
-  const unFormatted = 'console.log("hello world");';
+  const directory = temporaryDirectory({ prefix: 'hello-world' })
+  const unFormatted = 'console.log("hello world");'
 
-  await fs.writeFile(path.join(directory, "index.js"), unFormatted);
+  await fs.writeFile(path.join(directory, 'index.js'), unFormatted)
 
   const { stdout } = await execa(name, [], {
     cwd: directory,
@@ -18,27 +18,27 @@ test(name, async () => {
       // @ts-expect-error
       FORCE_COLOR: 2,
     },
-  });
+  })
 
   const formattedFile = await fs.readFile(
-    path.join(directory, "index.js"),
-    "utf-8"
-  );
-  expect(formattedFile).toEqual("console.log('hello world')\n");
+    path.join(directory, 'index.js'),
+    'utf-8',
+  )
+  expect(formattedFile).toEqual("console.log('hello world')\n")
 
   const packageJson = (await readPackage({
     cwd: directory,
     normalize: false,
   })) as PackageJson & {
-    prettier: string;
-    devDependencies: Record<string, string>;
-  };
+    prettier: string
+    devDependencies: Record<string, string>
+  }
 
-  expect(packageJson.prettier).toEqual("prettier-config-one");
+  expect(packageJson.prettier).toEqual('prettier-config-one')
   expect(Object.keys(packageJson.devDependencies)).toEqual([
-    "prettier",
-    "prettier-config-one",
-  ]);
-  console.log("stdout", stdout);
-  expect(stdout).toMatchSnapshot();
-});
+    'prettier',
+    'prettier-config-one',
+  ])
+  console.log('stdout', stdout)
+  expect(stdout).toMatchSnapshot()
+})
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,30 @@
 #!/usr/bin/env node
 
-import { execa } from "execa";
-import chalk from "chalk";
-import { readPackage } from "read-pkg";
-import sortPackageJson from "sort-package-json";
-import { writePackage } from "write-pkg";
+import { execa } from 'execa'
+import chalk from 'chalk'
+import { readPackage } from 'read-pkg'
+import sortPackageJson from 'sort-package-json'
+import { writePackage } from 'write-pkg'
 
-console.log();
-console.log(chalk.blue.bold(" setup-prettier"));
-console.log();
+console.log()
+console.log(chalk.blue.bold(' setup-prettier'))
+console.log()
 
-await execa("npm", [
-  "install",
-  "prettier",
-  "prettier-config-one",
-  "--save-dev",
-]);
+await execa('npm', ['install', 'prettier', 'prettier-config-one', '--save-dev'])
 
 const packageJson = await readPackage({
   normalize: false,
-});
+})
 
 const updatedPackageJson = sortPackageJson({
   ...packageJson,
-  prettier: "prettier-config-one",
-});
+  prettier: 'prettier-config-one',
+})
 
 // @ts-expect-error - sort-package-json doesn't return a compatible type
-await writePackage(updatedPackageJson);
+await writePackage(updatedPackageJson)
 
-await execa("npx", ["prettier", "--write", "."]);
+await execa('npx', ['prettier', '--write', '.'])
 
-console.log(` ${chalk.green("prettier and prettier-config-one added!")}`);
-console.log();
+console.log(` ${chalk.green('prettier and prettier-config-one added!')}`)
+console.log()
